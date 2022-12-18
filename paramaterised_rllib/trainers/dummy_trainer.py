8cc6739e80efd0afee5cbe69662c5ce797337552
Alex Collins <alexxcollins@googlemail.com>
2022-12-18 16:44:36 +0100

    removed lines from setup.py

--- paramaterised_rllib/trainers/dummy_trainer.py
+++ paramaterised_rllib/trainers/dummy_trainer.py
@@ -14,7 +14,7 @@ class DummyTrainer:
         if all_actions:
             # if agent randomly chooses across whole space including pickup, drop, toggle then it will very likely
             # take a very long time on simple tasks and complex tasks are probably futile anyway.
-            return np.random.randint(env.acition_space.n - 1)  # Discrete(n) -> return rand int between 0 and n-1 (incl. n-1).
+            return np.random.randint(env.action_space.n - 1)  # Discrete(n) -> return rand int between 0 and n-1 (incl. n-1).
         else:
             # at least there is a chance of completing an empty room task
             return np.random.randint(3)  # Discrete(4) -> return rand int between 0 and 2 (incl. 2).
